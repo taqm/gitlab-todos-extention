f1c52fba057da57d3c985eeb0a77e1b70afadbc0
taqm <takumu@kare-pan.com>
2017-03-07 22:48:13 +0900

    upgrade popup

--- src/background/background.js
+++ src/background/background.js
@@ -34,14 +34,18 @@ function notification(data) {
   const newTodos = data.filter((it) => !known.includes(it.id));
   // 今回はじめて取得したものは通知する
   newTodos.forEach((todo) => {
+    let message = `${todo.target_type} / ${todo.action_name}\n`;
+    message += `from ${todo.author.name}\n`;
+    message += todo.body;
     chrome.notifications.create(''+todo.id, {
       type: 'basic',
-      iconUrl: '/icon.png',
+      iconUrl: '/icons/icon.png',
       title: 'GitLab Notification',
-      message: todo.body
+      message
     });
   });
   localStorage.setItem('knownTodo', JSON.stringify(data.map((it) => it.id)));
+  return data;
 }
 
 function polling() {
@@ -51,6 +55,9 @@ function polling() {
     Todo.load()
       .then(setBadgeText)
       .then(notification)
+      .then((data) => {
+        localStorage.todos = JSON.stringify(data);
+      })
       .fail((e) => {
         console.error(e);
         clearInterval(timer);
--- src/popup/script.js
+++ src/popup/script.js
@@ -1,2 +1,41 @@
-const gotopage = document.getElementById('gotopage');
-gotopage.href = localStorage.url;
+const todos = JSON.parse(localStorage.todos);
+
+const list = document.getElementById('todo-list');
+todos.forEach((todo) => {
+  const li = document.createElement('li');
+  li.classList.add('todo');
+  li.innerHTML = row(todo);
+  list.appendChild(li);
+});
+
+function row(todo) {
+  const created = dateFormat(new Date(todo.created_at));
+  return `
+    <div class="panel-heading">
+      ${todo.project.name_with_namespace}
+    </div>
+    <div class="panel-body">
+      <img class="avatar" src="${todo.author.avatar_url}"></img>
+      <div class="todo-box">
+        <div class="title">
+          ${todo.target_type} / ${todo.action_name}
+          <span class="author-name">${todo.author.name}</span>
+          ${created}
+        </div>
+        <div class="body">${todo.body}</div>
+      </div>
+    </div>
+    <a class="surface" target="_blank" href="${todo.target_url}">
+    </a>
+  `;
+}
+
+function dateFormat(date) {
+  const year = date.getFullYear();
+  const month =date.getMonth() + 1;
+  const day = date.getDate();
+  const hour = date.getHours();
+  const min = `00${date.getMinutes()}`.slice(-2);
+
+  return `${year}/${month}/${day} ${hour}:${min}`;
+}
